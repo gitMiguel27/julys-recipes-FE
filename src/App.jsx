aa6bc845975b5aa149b8bb2647baf9cde3f537b2
Miguel Nazario <95545880+gitMiguel27@users.noreply.github.com>
2025-01-28 10:06:39 -0500

    set up components folder, home page with header and footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'
+import Home from './components/Home/Home'
 
 function App() {
 
   return (
     <>
-      <h1>Hello Frontend</h1>
+      <Home />
     </>
   )
 }
---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from '../Header/Header'
+import Footer from '../Footer/Footer'
+
+function Home() {
+  return (
+    <>
+        <Header />
+        <div>This is the Home Page</div>
+        <Footer />
+    </>
+  )
+}
+
+export default Home
