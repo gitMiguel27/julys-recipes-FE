d903925619a5d4aa54bb177c7dfd07bd2e313799
Miguel Nazario <95545880+gitMiguel27@users.noreply.github.com>
2025-01-30 15:33:25 -0500

    home view recipes button navigate to all recipes

--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -1,11 +1,14 @@
 import { Button, Container } from 'react-bootstrap'
+import { useNavigate } from 'react-router-dom'
 
 function Welcome() {
+  const navigate = useNavigate()
+
   return (
     <Container className='my-lg-auto'>
         <h1>Welcome to July&apos;s Recipes</h1>
         <h3>Dishes inspired by my mom<br />Authentically, Peruvian</h3>
-        <Button variant='danger' >View Recipes</Button>
+        <Button variant='danger' onClick={() => navigate('/recipes')} >View Recipes</Button>
     </Container>
   )
 }
