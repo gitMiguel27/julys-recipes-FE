2c9eb1de9283cfa1c0d04a7d2247c2f15ff4e96f
Miguel Nazario <95545880+gitMiguel27@users.noreply.github.com>
2025-01-30 15:20:41 -0500

    mapping through ingredients list

--- src/pages/RecipePage/RecipePage.jsx
+++ src/pages/RecipePage/RecipePage.jsx
@@ -1,9 +1,31 @@
-import { Container } from 'react-bootstrap'
+import { Container, Row, Col, Image, ListGroup, ListGroupItem } from 'react-bootstrap'
 
 function RecipePage({ currentRecipe }) {
   return (
-    <Container style={{ height: '100vh' }}>
-        <h1>{currentRecipe.title}</h1>
+    <Container className='d-flex text-center' style={{ height: '200vh' }}>
+        <Row>
+            <Col xs={12} >
+                <h2 >{currentRecipe.title}</h2>
+            </Col>
+            <Col xs={12} >
+                <Image src={currentRecipe.image} alt={currentRecipe.title} fluid rounded />
+            </Col>
+            <Col xs={12} >
+                <h5>Ingredients:</h5>
+                <ListGroup as='ol' numbered >
+                    {
+                        currentRecipe.ingredients.map(ingredient => {
+                            return (
+                                <ListGroup.Item as='li' key={ingredient} >{ingredient}</ListGroup.Item>
+                            )
+                        })
+                    }
+                </ListGroup>
+            </Col>
+            <Col xs={12} >
+                <Image src={currentRecipe.image} alt={currentRecipe.title} fluid rounded />
+            </Col>
+        </Row>
     </Container>
   )
 }
