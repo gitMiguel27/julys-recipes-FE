59577678f1c7807f8c13e738f72938c244dc1604
Miguel Nazario <95545880+gitMiguel27@users.noreply.github.com>
2025-01-30 15:26:39 -0500

    added click event and routing to card image

--- src/components/RecipeCard/RecipeCard.jsx
+++ src/components/RecipeCard/RecipeCard.jsx
@@ -5,7 +5,9 @@ function RecipeCard({ recipe, setCurrentRecipe }) {
   return (
     <Col md={3} className='d-flex align-items-stretch' >
       <Card className='text-center border border-light rounded' >
-          <Card.Img variant="top" src={recipe.image} alt={recipe.title} />
+          <Nav.Link as={NavLink} to={`/${recipe._id}`}>
+            <Card.Img variant="top" src={recipe.image} alt={recipe.title} onClick={() => setCurrentRecipe(recipe)} />
+          </Nav.Link>
           <Card.Body className='align-content-end' >
               <Card.Text onClick={() => setCurrentRecipe(recipe)}>
                 <Nav.Link as={NavLink} to={`/${recipe._id}`}>{recipe.title}</Nav.Link>
